b49e20c9ce9ed2bf6fe797ec17eccbede4dc5e71
hoanghai <lamhoanghai2509@gmail.com>
2019-12-06 19:02:59 +0700

    Array Method

---
+++ buoi4.js
@@ -0,0 +1,25 @@
+// var number =5 //number
+// var name= "lam hoang hai" //string
+// var hai ={name:'hai',age:10} //object
+// var array  = [1,2,3,4,5,6]    //object
+// var specialundified= undefined  //undefined
+// var specialNull = null //object
+// var specialfunction = console.log //function
+// console.log(typeof specialfunction) 
+
+
+//13- Array Method
+const numbers = [1,2,3,4,5]
+// Them phan tu vao vi tri cuoi cung
+//numbers.push(6,7,8)
+// console.log(numbers)
+
+//Xoa phan tu vi tri cuoi cung
+//numbers.pop()
+
+//Them phan tu vi tri dau tien
+//numbers.unshift(-1,0)
+
+//Xoa phan tu vi tri dau tien
+ numbers.shift(2)
+ console.log(numbers)
